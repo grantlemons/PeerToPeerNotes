c630041defb69c9a5c8d7c9944306860059da301
kongmenglor <kongmeng.k.lor@gmail.com>
2024-03-06 16:27:26 -0700

    Revert "Another push for deploy."
    This reverts commit 873767555e803aceb6261ccd894419e33f781b46.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,10 +7,12 @@ const config = {
 
   kit: {
     adapter: adapter(),
-    paths: {
-          base: process.env.NODE_ENV === "production" ? "/kongmenglor.github.io" : "",
-      },
   },
+  prerender: {
+    onError: () => {
+      return "continue";
+    }
+  }
 };
 
 export default config;
